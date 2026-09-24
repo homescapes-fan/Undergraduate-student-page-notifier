0c556b7850cf40f4461ec83571dedaec544ed56c
homescapes-fan <263303903+homescapes-fan@users.noreply.github.com>
2026-09-25 00:34:14 +0900

    取得したURLをlog.csvに書き込む

---
+++ check.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import sync_playwright, TimeoutError
+from datetime import datetime
+
+URL = "https://sp.ritsumei.ac.jp/studentportal/s/student10"
+
+with sync_playwright() as p:
+    success = True
+    try:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(storage_state="auth.json")
+        page = context.new_page()
+
+        page.goto(URL)
+        page.wait_for_url(URL+"**", timeout=25000)
+    except TimeoutError:
+        success = False
+
+    if success:
+        print(page.url)
+    else:
+        print("失敗")
+
+    time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    url = page.url.split("?")[0]
+
+    line = f"{time}, {success}, {url}"
+    print(line)
+
+    with open("log.csv", "a", encoding="utf-8") as f:
+        f.write(line+"\n")
+
